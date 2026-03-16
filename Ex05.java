1539b18c4f2a55fcb10a313e7f75f57cf177fee4
Vinicius Alencar <vinicius.alencar@edu.pucrs.br>
2026-03-16 23:52:23 +0100

    Exercicio 5

--- Ex05.java
+++ Ex05.java
@@ -7,11 +7,11 @@ public class Ex05 {
         if (str.length() == 0 || str.length() == 1)
             return true;
 
-        if (str.charAt(0) == str.charAt(str.length()-1)){
-            if (str.length() >= 2)
-                return isPal(str.substring(1,str.length()-1));
-        }
-        return false;
+        if (str.charAt(0) != str.charAt(str.length()-1))
+            return false;
+
+        return isPal(str.substring(1,str.length()-1));
+        
     }
     public static void main(String[] args) {
         
